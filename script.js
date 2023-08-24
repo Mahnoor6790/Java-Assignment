ddcd81ddc4880d8aa96f9a105db5332dc6351379
Mahnoor6790 <mahnoormalik6790@gmail.com>
2023-08-24 10:28:09 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+// Find maximum Number
+/*let array = [78,9,54,2,45,77,56,4,9,7];
+let max = Math.max.apply(null,array);
+console.log(max);*/
+
+// Find minimum Number
+/*let array = [78,9,54,2,45,77,56,4,9,7];
+let min = Math.min.apply(null,array);
+console.log(min);*/
+
+//Find 2nd maximum number
+/*let array = [8,3,4,9,34,22,36,78];
+array.sort((a,b)=> a-b);
+console.log('2nd largest number: ' + array[array.length-2]);*/
+
+//Find 3rd maximum number
+/*let array = [8,3,4,9,34,22,36,78];
+array.sort((a,b)=> a-b);
+console.log('3rd largest number: ' + array[array.length-3]);*/
+
+//Find 4th maximum number
+/*let array = [8,3,4,9,34,22,36,78];
+array.sort((a,b)=> a-b);
+console.log('4th largest number: ' + array[array.length-4]);*/
+
+//Find 2nd minimum number
+/*let array = [8,3,4,9,34,22,36,78];
+array.sort((a,b)=> a-b);
+console.log('2nd smallest number :' + array[1]);*/
+
+//Find 3nd minimum number
+/*let array = [8,3,4,9,34,22,36,78];
+array.sort((a,b)=> a-b);
+console.log('3nd smallest number :' + array[2]);*/
+
+//Find 4th minimum number
+let array = [8,3,4,9,34,22,36,78];
+array.sort((a,b)=> a-b);
+console.log('4th smallest number :' + array[3]);
+
+
+
+
+
